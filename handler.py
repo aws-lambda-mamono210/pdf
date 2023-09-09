f51bc6ce7e057f9fe3007409b07f02a3786a5f0f
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2023-09-09 02:07:55 +0000

    S3 バケットを指定して PDF ファイルを結合する機能を追加した refs #3901

---
+++ handler.py
@@ -0,0 +1,27 @@
+import os
+from common_modules.pdf_merger.pdf_merger import merge_pdfs_in_directory
+from common_modules.pdf_pager.pdf_pager import add_page_numbers
+from common_modules.s3_utils.s3_utils import S3Utils
+
+def handler(bucket_name, object_keys, output_directory, output_filename, merged_filename):
+    # Step 1: Initialize the S3 client
+    s3_client = S3Utils(bucket_name)
+
+    # Step 2: Download the objects
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for object_key in object_keys:
+        destination_path = os.path.join(output_directory, object_key)
+        s3_client.download_file(object_key, destination_path)
+
+    # Step 3: Merge the downloaded files
+    merge_pdfs_in_directory(output_directory, merged_filename)
+
+    # Step 4: Add page numbers to the merged file
+    add_page_numbers(merged_filename, output_filename)
+
+    # Step 5: Upload the merged file to S3
+    s3_client.upload_file(output_filename, output_filename)
+
+    print("Process completed successfully!")
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import boto3
 import shutil
 import unittest
 import sys
@@ -10,47 +11,55 @@ from PyPDF2 import PdfWriter
 from PyPDF2 import PdfReader
 from common_modules.pdf_merger.pdf_merger import merge_pdfs_in_directory
 from common_modules.pdf_utils.pdf_utils import create_pdf_with_text
+from handler import handler
 
-class TestPDFMerger(unittest.TestCase):
+class TestHandler(unittest.TestCase):
 
     def setUp(self):
-        # 前回のテストで作成されたPDFファイルを削除
-        save_dir = "artifacts"  # 保存先ディレクトリのパスを指定
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        for filename in os.listdir(save_dir):
-            if filename.endswith('.pdf'):  # PDFファイルのみを対象とする
-                os.remove(os.path.join(save_dir, filename))
+        # Setup necessary variables for the tests
+        self.bucket_name = 'mamono210'
+        self.object_keys = ['aws/lambda/pdf/test_file1.pdf', 'aws/lambda/pdf/test_file2.pdf']  # Replace with your test file names
+        self.output_directory = 'test_downloaded_files'
+        self.output_filename = 'test_output_with_page_numbers.pdf'
+        self.merged_filename = 'test_merged_output.pdf'
 
-        # テスト用の一時ディレクトリを作成
-        self.test_dir = tempfile.mkdtemp()
+        os.makedirs(os.path.join(self.output_directory, 'aws/lambda/pdf'), exist_ok=True)
 
-        # サンプルのPDFファイルを3つ作成
-        for i in range(3):
-            file_path = os.path.join(self.test_dir, f"{i+1:02}_test.pdf")
-            create_pdf_with_text(f"Page {i+1}", file_path)
+        # Create test PDF files
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory)
+
+        # Step 1: Create test files locally
+        create_pdf_with_text("Test Content for File 1", os.path.join(self.output_directory, 'test_file1.pdf'))
+        create_pdf_with_text("Test Content for File 2", os.path.join(self.output_directory, 'test_file2.pdf'))
+
+        # Step 2: Upload test files to S3
+        s3_client = boto3.client('s3')
+        s3_client.upload_file(os.path.join(self.output_directory, 'test_file1.pdf'), self.bucket_name, self.object_keys[0])
+        s3_client.upload_file(os.path.join(self.output_directory, 'test_file2.pdf'), self.bucket_name, self.object_keys[1])
+
+    def test_handler(self):
+        # Test the handler function
+        handler(self.bucket_name, self.object_keys, self.output_directory, self.output_filename, self.merged_filename)
+
+        # Check if the output file is created
+        self.assertTrue(os.path.exists(self.output_filename))
+
+        # Check if the merged file is created
+        self.assertTrue(os.path.exists(self.merged_filename))
+
+        # Additional assertions can be added here to verify the content of the output files
 
     def tearDown(self):
-        # テストが終わったら、テスト用のPDFファイルを'artifacts'ディレクトリに移動
-        save_dir = "artifacts"
-        for filename in os.listdir(self.test_dir):
-            if filename.endswith('.pdf'):
-                shutil.move(os.path.join(self.test_dir, filename), os.path.join(save_dir, filename))
-        os.rmdir(self.test_dir)
+        # Clean up files created during the test
+        if os.path.exists(self.output_filename):
+            os.remove(self.output_filename)
 
-    def test_merge_pdfs(self):
-        output_file = os.path.join(self.test_dir, "merged.pdf")
+        if os.path.exists(self.merged_filename):
+            os.remove(self.merged_filename)
 
-        # マージ関数を呼び出す
-        merge_pdfs_in_directory(self.test_dir, output_file)
+        if os.path.exists(self.output_directory):
+           shutil.rmtree(self.output_directory)
 
-        # 結果を確認する
-        self.assertTrue(os.path.exists(output_file), "Merged PDF does not exist.")
-
-        # 3つのPDFをマージしたので、マージされたPDFには3ページ含まれているはず
-        with open(output_file, "rb") as f:
-            reader = PdfReader(f)
-            self.assertEqual(len(reader.pages), 3, "Merged PDF does not have the expected number of pages.")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
